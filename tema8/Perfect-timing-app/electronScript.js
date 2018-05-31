79428c4054fa150102eb3bc5e1bf86f2c9616d92
Hedda Brodtkorb <hedda.brodtkorb@gmail.com>
2018-05-31 16:45:52 +0200

    Added icons - didn't activate yet

--- tema8/Perfect-timing-app/electronScript.js
+++ tema8/Perfect-timing-app/electronScript.js
@@ -1,17 +1,24 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, ipcMain, Notification} = require('electron')
+const {
+  app,BrowserWindow,ipcMain,Notification
+} = require('electron')
+
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow, popup;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-  
+
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
